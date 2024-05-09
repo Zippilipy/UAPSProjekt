f26bf766422fe8092a0216fa19c392b9b302eaeb
harald329 <haraldtoft4@gmail.com>
2024-05-09 18:11:40 +0200

    Ensures the matrix is invertible

--- inverse.py
+++ inverse.py
@@ -2,13 +2,24 @@
 import time
 import numpy as np
 
-DIMENSION = 100
+DIMENSION = 20
 
 start_time = time.time()
 
 # Calculates and prints an array
 A = np.array([np.random.rand(DIMENSION,DIMENSION)])
-matrix = np.linalg.inv(A)
+print(A)
+
+# Finds the inverse of a matrix
+determinant = 0
+
+# ensures that the matrix is inversible
+while determinant == 0:
+	matrix = np.linalg.inv(A)
+	determinant = np.linalg.det(matrix)
+	if determinant != 0:
+		print("determinant is ", determinant)
+
 
 end_time = time.time()
 elapsed_time = end_time - start_time
