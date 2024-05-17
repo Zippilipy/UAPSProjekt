091ba29765174f9077d019a08b3c6668e23c165f
Harald Toft <113995366+harald329@users.noreply.github.com>
2024-05-17 13:09:22 +0200

    Update inverse.py
    can run multiple times

--- inverse/inverse.py
+++ inverse/inverse.py
@@ -1,22 +1,28 @@
 """ Finds an invertible matrix, then inverts it"""
+import sys
 import time
 import numpy as np
+
+times = int(sys.argv[1])
 
 DIMENSION = 100
 MIN = 0
 MAX = 100
 
-start_time = time.time_ns()
+i = 0
+while i < times:
+    start_time = time.time_ns()
 
-DETERMINANT = 0
+    DETERMINANT = 0
 
-while DETERMINANT == 0:
-    matrix = np.array([np.random.randint(MIN,MAX, size=(DIMENSION,DIMENSION))])
-    DETERMINANT = np.linalg.det(matrix)
-    inverse = np.linalg.inv(matrix)
+    while DETERMINANT == 0:
+        matrix = np.array([np.random.randint(MIN,MAX, size=(DIMENSION,DIMENSION))])
+        DETERMINANT = np.linalg.det(matrix)
+        inverse = np.linalg.inv(matrix)
 
-end_time = time.time_ns()
-elapsed_time = end_time - start_time
+    end_time = time.time_ns()
+    elapsed_time = end_time - start_time
 
-# Prints end time
-print(elapsed_time)
+    # Prints end time
+    print(elapsed_time)
+    i = i + 1
